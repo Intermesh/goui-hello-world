9bf003d723c883b71ca345f2ce6ee8b397b0be23
Merijn Schering <mschering@intermesh.nl>
2023-02-28 22:30:59 +0100

    Tree shaking

--- script/Index.ts
+++ script/Index.ts
@@ -1,14 +1,13 @@
-import { btn, comp, root } from "@intermesh/goui";
-
+import {comp, root} from "@intermesh/goui";
 
 root.items.add(
 	comp({tagName: "h1", text: "Hello World!"}),
 	comp({tagName: "p", text: "Welcome to the simplest GOUI application."}),
-	btn({
-		text: "Visit docs",
-		cls: "primary",
-		handler: () => window.open("https://www.group-office.com")
-	})
+	// btn({
+	// 	text: "Visit docs",
+	// 	cls: "primary",
+	// 	handler: () => window.open("https://www.group-office.com")
+	// })
 
 );
 
