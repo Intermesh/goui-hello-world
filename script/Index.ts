76d58a570120a72a32103714cc98aced1340c87b
Merijn Schering <mschering@intermesh.nl>
2023-03-13 12:49:33 +0100

    Optimizations

--- script/Index.ts
+++ script/Index.ts
@@ -1,13 +1,13 @@
-import {comp, root} from "@intermesh/goui";
+import {btn, comp, root} from "@intermesh/goui";
 
 root.items.add(
 	comp({tagName: "h1", text: "Hello World!"}),
 	comp({tagName: "p", text: "Welcome to the simplest GOUI application."}),
-	// btn({
-	// 	text: "Visit docs",
-	// 	cls: "primary",
-	// 	handler: () => window.open("https://www.group-office.com")
-	// })
+	btn({
+		text: "Visit docs",
+		cls: "primary",
+		handler: () => window.open("https://github.com/intermesh/goui-docs/")
+	})
 
 );
 
